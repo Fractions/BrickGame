67dea95d25ebba9e7d3b08565c784458f06d302a
turnburn <bcturner@Brandons-MacBook-Pro.local>
2014-12-09 20:05:11 -0500

    build

--- src/brickgame/BrickGameForm.java
+++ src/brickgame/BrickGameForm.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package brickgame;
 import java.awt.Color;
 import java.io.*;
@@ -17,15 +12,10 @@ import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.*;
 
 
-/**
- *
- * @author brandonturner
- */
+
 public class BrickGameForm extends javax.swing.JFrame {
 
-    /**
-     * Creates new form BrickGameForm
-     */
+    
     int score = 5;
     String filePath; 
     Random gen = new Random();
